183b4889c42f51701dc4720c5cd09d704a07067d
joesatow <joseph.satow@gmail.com>
2023-01-09 06:51:47 -0500

    changes

--- helper_functions/calendarAPI_function.py
+++ helper_functions/calendarAPI_function.py
@@ -1,4 +1,10 @@
 import requests
+import pytz
+from datetime import datetime
+
+est = pytz.timezone('US/Eastern')
+utc = pytz.utc
+fmt = '%Y-%m-%d %H:%M'
 
 def callCalendarAPI(startDate, endDate):
     url = "https://financialmodelingprep.com/api/v3/economic_calendar?from=2023-01-05&to=2023-02-05&apikey=0fc22e02ecc68a84494f68c046d17097"
@@ -16,5 +22,27 @@ def callCalendarAPI(startDate, endDate):
         if result['impact'] != 'High':
             continue
 
+        # 2023-01-05 13:30:00
+        reportDateTime = result['date']
+        reportDate = reportDateTime.split(' ')[0].split('-') # ['2023','01','05]
+        reportTime = reportDateTime.split(' ')[1].split(':') # ['13','30','00']
+        reportYear = int(reportDate[0])
+        reportMonth = int(reportDate[1])
+        reportDay = int(reportDate[2])
+        reportHour = int(reportTime[0])
+        reportMinute = int(reportTime[1])
+
+        reportDatetimeObject = datetime(reportYear, reportMonth, reportDay, reportHour, reportMinute, tzinfo=utc)
+        
+        if result['event'] == "ADP Employment Change (Dec)":
+            x = 5
+
+        if reportDatetimeObject.hour != 13 or reportDatetimeObject.minute != 30:
+            continue
+
+        reportDatetimeEST = reportDatetimeObject.astimezone(est).strftime(fmt)
+
         print(f"Event: {result['event']}")
-        print(f"Date: {result['date']}")
+        print(f"Date: {reportDatetimeEST}")
+        print()
+
