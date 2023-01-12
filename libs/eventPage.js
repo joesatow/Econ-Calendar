2990f94641a410d428a0dd4ecf1bf5f80fc0a900
joesatow <98419512+joesatow@users.noreply.github.com>
2023-01-12 16:01:33 -0500

    push

--- libs/eventPage.js
+++ libs/eventPage.js
@@ -4,48 +4,38 @@ mktEvent = urlParams.get("event")
 mktEventToString = mktEvent.replace(/_/g, " ")
 console.log(mktEvent)
 
-var today = new Date();
-var monday = getMonday(today)
-var friday = new Date();
-var dayAfterFriday = new Date();
-friday.setDate(monday.getDate() + 4)
-dayAfterFriday.setDate(friday.getDate() + 1)
-window.onload = function() {
-    document.getElementById("pageHeader").innerHTML = mktEventToString
-}
+var today = new Date()
+var currentRangeStart = new Date()
+var currentRangeEnd = new Date()
+currentRangeStart.setDate(today.getDate() - 31)
+currentRangeEnd.setDate(today.getDate() - 1)
+var twoYearsAgo = new Date()
+twoYearsAgo.setDate(today.getDate() - 730)
+console.log(today)
+console.log(twoYearsAgo)
 
-function getMonday(d) {
-    d = new Date(d);
-    var day = d.getDay(),
-        diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
-    return new Date(d.setDate(diff));
-}
-  
-function dateToYMD(date) {
-    var d = date.getDate();
-    var m = date.getMonth() + 1; //Month from 0 to 11
-    var y = date.getFullYear();
-    return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
-}
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
+while (currentRangeEnd > today) {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+        };
+        
+    var tomorrow = new Date(today)
+    var dayAfterTomorrow = new Date(tomorrow)
+    tomorrow.setDate(tomorrow.getDate() + 1) // start date for API call, tomorrow
+    dayAfterTomorrow.setDate(tomorrow.getDate() + 2) // end date for API call
     
-var tomorrow = new Date(today)
-var dayAfterTomorrow = new Date(tomorrow)
-tomorrow.setDate(tomorrow.getDate() + 1) // start date for API call, tomorrow
-dayAfterTomorrow.setDate(tomorrow.getDate() + 2) // end date for API call
+    tomorrowToString = dateToYMD(tomorrow)
+    dayAfterTomorrowToString = dateToYMD(dayAfterTomorrow)
+    
+    fetch(`https://financialmodelingprep.com/api/v3/economic_calendar?from=${dateToYMD(currentRangeStart)}&to=${dateToYMD(currentRangeEnd)}&apikey=0fc22e02ecc68a84494f68c046d17097`, requestOptions)
+    .then(response => response.text())
+    .then(result => readResult(result))
+    .catch(error => console.log('error', error));
 
-tomorrowToString = dateToYMD(tomorrow)
-dayAfterTomorrowToString = dateToYMD(dayAfterTomorrow)
-
-fetch(`https://financialmodelingprep.com/api/v3/economic_calendar?from=${dateToYMD(monday)}&to=${dateToYMD(dayAfterFriday)}&apikey=0fc22e02ecc68a84494f68c046d17097`, requestOptions)
-.then(response => response.text())
-.then(result => readResult(result))
-.catch(error => console.log('error', error));
-
+    currentRangeStart.setDate(currentRangeStart.getDate() - 30)
+    currentRangeEnd.setDate(currentRangeEnd.getDate() - 30)
+}
 
 function readResult(response) {
     const jsonObj = JSON.parse(response)
@@ -60,9 +50,9 @@ function readResult(response) {
         
         var date = new Date(item['date'])
         
-        // if (date.getHours() != 13 || date.getMinutes() != 30){
-        //     return;
-        // }
-       
+        if (date.getHours() != 13 || date.getMinutes() != 30){
+            return;
+        }
+        
     })
 }
